e121049a135beed7ef98e6b2e97260a2d7fe1aef
AlexVT76 <tokarevalex76@gmail.com>
2024-10-09 17:12:26 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fake
+from true_math import divide as true
+result1=fake(6,3)
+result2=fake(4,0)
+result3=true(4,2)
+result4=true(3,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
